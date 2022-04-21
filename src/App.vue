33d3f495c3291fd9cd9fac54c35eda64f21d324b
jjubbu <hogugugut4102.off@gmail.com>
2022-04-21 23:32:56 +0900

    🌱 Start vue project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { reactive } from "vue";
+
+const count = reactive({ count: 0 });
+console.log(count.count);
+count.count++;
+</script>
+
+<template>hlllelllo</template>
+
+<style></style>
