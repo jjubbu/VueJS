56682cb232697784bc4d125b5db55703dde978a2
jjubbu <hogugugut4102.off@gmail.com>
2022-04-27 06:13:33 +0900

    ✅ Add initialStates in store

--- Todo-List/src/store/index.js
+++ Todo-List/src/store/index.js
@@ -1,7 +1,29 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
+  state: {
+    todoList: [
+      {
+        id: 0,
+        title: "제목이다!",
+        comment:
+          "마라탕에 푸주, 두부피, 죽순, 어묵만 가득 채워서 먹어보고싶다.",
+        done: false,
+      },
+      {
+        id: 1,
+        title: "제목이다!제목이다!",
+        comment: "영어공부를 좀 혀야하는디 에혀 우짜냐",
+        done: false,
+      },
+      {
+        id: 2,
+        title: "제목이다!제목이다!제목이다!",
+        comment: "뷰 생각보다 재밌다 하지만 여전히 리액트가 너무 좋은.",
+        done: false,
+      },
+    ],
+  },
   getters: {},
   mutations: {},
   actions: {},
