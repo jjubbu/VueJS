7929272533708ca787689a510a6fefe69d29e4bf
seona-shim <hogugu4102@codebrick.co>
2022-04-26 18:03:25 +0900

    Merge branch 'main' of https://github.com/jjubbu/VueJS

--- UdemyVue/basics-05-using-the-native-event-object/app.js
+++ UdemyVue/basics-05-using-the-native-event-object/app.js
@@ -2,12 +2,22 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: "",
     };
   },
+  computed: {
+    valueFunction() {
+      if (!this.name) {
+        console.log("hello");
+        return "";
+      }
+      return "hello " + this.name;
+    },
+  },
+
   methods: {
     setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
+      this.name = event.target.value + " " + lastName;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -15,8 +25,8 @@ const app = Vue.createApp({
     reduce(num) {
       this.counter = this.counter - num;
       // this.counter--;
-    }
-  }
+    },
+  },
 });
 
-app.mount('#events');
+app.mount("#events");
