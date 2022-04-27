f0323e184f50fba837c8400b9617434ee4b095af
jjubbu <hogugugut4102.off@gmail.com>
2022-04-28 01:37:37 +0900

    ✅ Add button, input element components

---
+++ Todo-List/src/components/InputElement.vue
@@ -0,0 +1,14 @@
+<template>
+  <label>
+    <h3>{{ title }}</h3>
+    <input />
+  </label>
+</template>
+<script>
+export default {
+  name: "InputElement",
+  props: {
+    title: String,
+  },
+};
+</script>
