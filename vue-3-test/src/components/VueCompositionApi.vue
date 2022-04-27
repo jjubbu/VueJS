331fdef9099c76edf2dd06ceba2c2734908c79f5
seona-shim <hogugu4102@codebrick.co>
2022-04-27 19:02:54 +0900

    ✅ Add composition api test component

---
+++ vue-3-test/src/components/VueCompositionApi.vue
@@ -0,0 +1,9 @@
+// * script setup 은 composition api 의...일부?
+<script setup>
+import { ref } from "vue";
+const reactivity = ref("hello");
+</script>
+
+<template>
+  <div>{{ reactivity }}</div>
+</template>
--- vue-3-test/src/views/HomeView.vue
+++ vue-3-test/src/views/HomeView.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <vue-composition-api></vue-composition-api>
   </div>
 </template>
 
 <script>
+import VueCompositionApi from "../components/VueCompositionApi.vue";
 export default {
   name: "HomeView",
-  components: {},
+  components: { VueCompositionApi },
 };
 </script>
