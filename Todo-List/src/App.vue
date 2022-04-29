2901228ce3fc5e4cf1f7d682f59f8caae830a5f8
jjubbu <hogugugut4102.off@gmail.com>
2022-04-29 23:16:16 +0900

    ✅ Add script setup

--- Todo-List/src/App.vue
+++ Todo-List/src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <todo-main></todo-main>
+  <button-element @click="showCard">Add card</button-element>
+  <add-card v-if="addCard"></add-card>
 </template>
 
 <script>
+import AddCard from "./components/AddCard.vue";
+import ButtonElement from "./components/ButtonElement.vue";
 import TodoMain from "./components/TodoMain.vue";
 export default {
   name: "App",
-  components: { TodoMain },
+  components: { TodoMain, AddCard, ButtonElement },
+};
+</script>
+<script setup>
+import { ref } from "vue";
+const addCard = ref(false);
+const showCard = () => {
+  addCard.value = true;
 };
 </script>
 
@@ -19,5 +30,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  section {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.257);
+    max-width: 480px;
+    margin: 20px auto;
+    padding: 20px;
+  }
+}
+</style>
+<style scoped>
+section {
 }
 </style>
--- Todo-List/src/components/InputElement.vue
+++ Todo-List/src/components/InputElement.vue
@@ -1,14 +1,18 @@
 <template>
   <label>
-    <h3>{{ title }}</h3>
+    <h3>{{ props.title }}</h3>
     <input />
   </label>
 </template>
 <script>
 export default {
   name: "InputElement",
-  props: {
-    title: String,
-  },
 };
+</script>
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  title: String,
+});
 </script>
